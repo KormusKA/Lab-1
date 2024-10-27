01dee989d3d5ea1ff199100914b97505b8858cd7
KormusKA <kormushkina2006@gmail.com>
2024-10-27 18:42:29 +0300

    Update and rename lab1 to lab1.py

--- lab1.py
+++ lab1.py
@@ -3,14 +3,15 @@ import time
 SET_COLOR = "\x1b[48;5;"
 END = "\x1b[0m"
 CLEAR = "\033[H"
-red = 1
-yellow = 221
-green = 35
-white = 231
-black = 0
+MOVING = "\x1b"
+RED = 1
+YELLOW = 221
+GREEN = 35
+WHITE = 231
+BLACK = 0
 
-def draw_line(offset=0, lenght=1, color=yellow):
-    line = "  " * lenght
+def draw_line(offset=0, length=1, color=YELLOW):
+    line = "  " * length
     print(f"{'  ' * offset}{SET_COLOR}{color}m{line}{END}")
 
 def draw_flag():
@@ -18,14 +19,14 @@ def draw_flag():
 
     for i in range(height):
         if i < height//2:
-            draw_line(lenght=10, offset=6)
+            draw_line(length=10, offset=6)
         else:
-            draw_line(lenght=10, offset=6, color=red)
+            draw_line(length=10, offset=6, color=RED)
 
-    print(f"\x1b[{height+1}A")
+    print(f"{MOVING}[{height+1}A")
 
     for i in range(height):
-        draw_line(lenght=6, color=green)
+        draw_line(length=6, color=GREEN)
 
 def draw_pattern():
     size = 8
@@ -39,38 +40,39 @@ def draw_pattern():
     for i in range(3):
         for j in range(2, -1, -1):
             if j != 2:
-                print(f"\x1b[{size+2}A")
+                print(f"{MOVING}[{size + 2}A")
 
             for line in range(size+1):
-                draw_line(offset + j*size, length, color)
+                draw_line(offset + (j * size), length, color)
 
                 if line < center:
                     offset -= step
-                    length += step*2
+                    length += step * 2
 
                 else:
                     offset += step
-                    length -= step*2
+                    length -= step * 2
 
             step = 1
             length = 1
             center = size // 2
             offset = size // 2
 
-        print(f"\x1b[{2}A")
+        print(f"{MOVING}[{2}A")
     print("\n")
 
-def draw_goafic():
+def draw_graph():
     print("y = x + 1")
 
     for i in range(9, 0, -1):
-        print(f"{i}{SET_COLOR}{white}m{(i-1) * '  '}{SET_COLOR}{black}m{'  '}{SET_COLOR}{white}m{(9-i) * '  '}{END}")
+        print(f"{i}{SET_COLOR}{WHITE}m{(i-1) * '  '}{SET_COLOR}{BLACK}m{'  '}{SET_COLOR}{WHITE}m{(9-i) * '  '}{END}")
     
-    print(f"0{SET_COLOR}{white}m{9 * '  '}{END}")
+    print(f"0{SET_COLOR}{WHITE}m{9 * '  '}{END}")
     print("  0 1 2 3 4 5 6 7 8")
             
 def draw_diogram():
-    numbers = [float(x) for x in open('sequence.txt')]
+    with open('sequence.txt') as file:
+        numbers = [float(x) for x in file]
     positive_num = 0
     negative_num = 0
 
@@ -80,11 +82,18 @@ def draw_diogram():
         if -5 <= i <= 0:
             negative_num += 1
     
-    len_pos = int(positive_num//10) if (positive_num - (positive_num//10)) < 0.5 else int(positive_num//10) + 1
-    len_neg = int(negative_num//10) if (negative_num - (negative_num//10)) < 0.5 else int(negative_num//10) + 1
+    if (positive_num - (positive_num // 10)) < 0.5:
+        len_pos = int(positive_num // 10) 
+    else:
+        len_pos = int(positive_num // 10) + 1
 
-    print(f"{SET_COLOR}{green}m{len_pos * '  '}{END} - числа от 0 до 5") 
-    print(f"{SET_COLOR}{red}m{len_neg * '  '}{END} - числа от -5 до 0") 
+    if (negative_num - (negative_num // 10)) < 0.5: 
+        len_neg = int(negative_num // 10)
+    else:
+        len_neg = int(negative_num // 10) + 1
+
+    print(f"{SET_COLOR}{GREEN}m{len_pos * '  '}{END} - числа от 0 до 5") 
+    print(f"{SET_COLOR}{RED}m{len_neg * '  '}{END} - числа от -5 до 0") 
 
 def animation():
     size = 15
@@ -110,8 +119,8 @@ def animation():
                         offset += step
                         length -= step*2
 
-                print(f"\x1b[{size+2}A")
-                print(f"\x1b[{offset}D")
+                print(f"{MOVING}[{size+2}A")
+                print(f"{MOVING}[{offset}D")
 
                 time.sleep(1.5)
 
@@ -128,7 +137,7 @@ if __name__ == "__main__":
     draw_pattern()
     print("\n")
 
-    draw_goafic()
+    draw_graph()
     print("\n")
     
     draw_diogram()
